a78293035ecfbe823adbe8a11dcfbcf0d7aa3fd2
aaryamannchallani <43716372+aaryamannchallani@users.noreply.github.com>
2020-06-09 14:38:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ const router = require('./router.js')
 
 app.use(router.routes());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 
 
 app.listen(process.env.PORT || 3000);
